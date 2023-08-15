cfc610f14b4b3849d09f13ef1fe56ba4d11ff244
Robert Wenzel <robert.wenzel@eas.iis.fraunhofer.de>
2023-08-15 17:48:13 +0200

    0.0.2

--- main.py
+++ main.py
@@ -5,9 +5,6 @@ Created on Fri Aug 11 12:57:12 2023
 @author: rwenzel
 """
 
-import sys
-sys.path.append(r'C:\Users\rwenzel\Desktop\250122_Python_Projekt\StockHero_pypi_0_4_3\src')
-
 from GUI.GUI import GUI
 
 def main():
